a819aac4d0bc9abc88bab6da4ad1b2d1af516ac5
Walaa Yazji <88341379+walaayazji@users.noreply.github.com>
2024-11-02 17:13:45 +0200

    Update MultiplicationTable.cs

--- Assets/scripts/MultiplicationTable.cs
+++ Assets/scripts/MultiplicationTable.cs
@@ -7,17 +7,14 @@ public class MultiplicationTable : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        for (int i= 1; i<=10; i++){
+        int j= Multiply(5,i);
+        Debug.Log($"5 * {i} = {j}");
+        }
+    }
+
         int  Multiply (int number1, int number2){
         int sum = number1 * number2;
         return sum;
         }
-
-        for (int i= 1; i<=10; i++){
-        int j= Multiply(5,i);
-        Debug.Log($"5 * {i} = {j}");
-
-    }
-
-
-    }
 }
