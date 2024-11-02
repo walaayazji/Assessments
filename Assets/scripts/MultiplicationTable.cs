16c023d0a95a82c016368f278bb308c3247e7f52
Walaa Alyazji <walaayazji@gmail.com>
2024-11-02 18:00:13 +0200

    NEW UPDATE

--- Assets/scripts/MultiplicationTable.cs
+++ Assets/scripts/MultiplicationTable.cs
@@ -4,20 +4,17 @@ using UnityEngine;
 
 public class MultiplicationTable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   
+    int  Multiply (int number1, int number2)    
     {
-        int  Multiply (int number1, int number2){
         int sum = number1 * number2;
         return sum;
-        }
-
+    }
+    void Start()
+    { 
         for (int i= 1; i<=10; i++){
         int j= Multiply(5,i);
         Debug.Log($"5 * {i} = {j}");
-
     }
-
-
     }
 }
