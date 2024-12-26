f194baad3a7eed657c53d65a1952459e2fe71256
Walaa Alyazji <walaayazji@gmail.com>
2024-12-26 11:53:54 +0200

    ADD NAMESPACE

--- Assets/scripts/Assignment 24/RacingGame.cs
+++ Assets/scripts/Assignment 24/RacingGame.cs
@@ -1,44 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
-using Assignment24;
 using UnityEngine;
 
-public class RacingGame : MonoBehaviour
+namespace Assignment24
 {
-    public RaceState raceState;
-    public void SimulateRach()
+    public class RacingGame : MonoBehaviour
     {
-        switch (raceState)
+        public RaceState raceState;
+        public void SimulateRach()
         {
-            case RaceState.Start:
-            Debug.Log("The race is about to begin. Get ready!");
-            break;
+            switch (raceState)
+            {
+                case RaceState.Start:
+                Debug.Log("The race is about to begin. Get ready!");
+                break;
 
-            case RaceState.Accelerate :
-            Debug.Log("You press the gas pedal. The car speeds up!");
-            break;
+                case RaceState.Accelerate :
+                Debug.Log("You press the gas pedal. The car speeds up!");
+                break;
 
-            case RaceState. Turn :
-            Debug.Log("You approach a sharp turn. Be careful not to crash!");
-            break;
+                case RaceState. Turn :
+                Debug.Log("You approach a sharp turn. Be careful not to crash!");
+                break;
 
-            case RaceState. Crash :
-            Debug.Log("You hit a barrier. The race is over.");
-            break;
+                case RaceState. Crash :
+                Debug.Log("You hit a barrier. The race is over.");
+                break;
 
-            case RaceState.Finish :
-            Debug.Log("You crossed the finish line. Well done!");
-            break;
+                case RaceState.Finish :
+                Debug.Log("You crossed the finish line. Well done!");
+                break;
 
-            default :
-            Debug.Log("This state is not defined. Something is wrong.");
-            break;
+                default :
+                Debug.Log("This state is not defined. Something is wrong.");
+                break;
 
+            }
         }
-    }
 
-    void Update()
-    {
-        SimulateRach();
+        void Update()
+        {
+            SimulateRach();
+        }
     }
 }
