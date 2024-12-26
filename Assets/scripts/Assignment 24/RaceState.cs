4e1392fbad26b3c5064f9c940bfc955aadf91808
Walaa Alyazji <walaayazji@gmail.com>
2024-12-26 11:05:28 +0200

    Assignment24

---
+++ Assets/scripts/Assignment 24/RaceState.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assignment24
+{
+
+    public enum RaceState 
+    {
+        Start,
+        Accelerate,
+        Turn,
+        Crash,
+        Finish,
+    }
+}
---
+++ Assets/scripts/Assignment 24/RacingGame.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Assignment24;
+using UnityEngine;
+
+public class RacingGame : MonoBehaviour
+{
+    public RaceState raceState;
+    public void SimulateRach()
+    {
+        switch (raceState)
+        {
+            case RaceState.Start:
+            Debug.Log("The race is about to begin. Get ready!");
+            break;
+
+            case RaceState.Accelerate :
+            Debug.Log("You press the gas pedal. The car speeds up!");
+            break;
+
+            case RaceState. Turn :
+            Debug.Log("You approach a sharp turn. Be careful not to crash!");
+            break;
+
+            case RaceState. Crash :
+            Debug.Log("You hit a barrier. The race is over.");
+            break;
+
+            case RaceState.Finish :
+            Debug.Log("You crossed the finish line. Well done!");
+            break;
+
+            default :
+            Debug.Log("This state is not defined. Something is wrong.");
+            break;
+
+        }
+    }
+
+    void Update()
+    {
+        SimulateRach();
+    }
+}
